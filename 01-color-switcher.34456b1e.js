911b4fbeff0641a41c8341c0aae6079a67c7bd28
marbiem <122818274+marbiem@users.noreply.github.com>
2023-03-12 21:45:47 +0000

    Deploying to gh-pages from @ marbiem/goit-js-hw-08@cc63016bab0d3f8c158fd4bb1efa507ef471de82 🚀

---
+++ 01-color-switcher.34456b1e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]"),e=document.querySelector("body");t.addEventListener("click",(()=>{console.log("hello"),e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`})),o.addEventListener("click",(()=>{}));
+//# sourceMappingURL=01-color-switcher.34456b1e.js.map
---
+++ 01-color-switcher.d0e477fe.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]"),e=document.querySelector("body");t.addEventListener("click",(function(){console.log("hello"),e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))})),o.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.d0e477fe.js.map
