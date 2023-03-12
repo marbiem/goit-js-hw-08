cc63016bab0d3f8c158fd4bb1efa507ef471de82
marbiem <biegun.marta@gmail.com>
2023-03-12 22:45:16 +0100

    Color changer initial changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeColor = () => {
+  console.log('hello');
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+const stopChangeColor = () => {};
+
+startButton.addEventListener('click', changeColor);
+stopButton.addEventListener('click', stopChangeColor);
